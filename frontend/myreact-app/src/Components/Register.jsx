df4ffda99defacfc3e3e65638c8d76459aea1a1e
upender-01 <bhukya.upendar.ece23@itbhu.ac.in>
2026-04-22 00:03:44 +0530

    Update Register.jsx

--- frontend/myreact-app/src/Components/Register.jsx
+++ frontend/myreact-app/src/Components/Register.jsx
@@ -8,7 +8,7 @@ export default function Register() {
 
   const handleRegister = async (e) => {
     e.preventDefault();
-    const res = await fetch('http://localhost:5000/api/v1/auth/register', {
+    const res = await fetch('https://scalable-rest-api-with-rbac-frontend-ifrn.onrender.com/api/v1/auth/register', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData)
