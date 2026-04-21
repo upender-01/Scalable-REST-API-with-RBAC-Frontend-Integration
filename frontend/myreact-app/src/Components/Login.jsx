1aebac202eeb5cfd73115fa96ba8faa0961a9474
upender-01 <bhukya.upendar.ece23@itbhu.ac.in>
2026-04-22 00:04:18 +0530

    Update Login.jsx

--- frontend/myreact-app/src/Components/Login.jsx
+++ frontend/myreact-app/src/Components/Login.jsx
@@ -10,7 +10,7 @@ export default function Login() {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch('http://localhost:5000/api/v1/auth/login', {
+      const response = await fetch('https://scalable-rest-api-with-rbac-frontend-ifrn.onrender.com/api/v1/auth/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password })
