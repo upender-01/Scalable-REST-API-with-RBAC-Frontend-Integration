3753d9a5156e4e2f566a09e776a8d3b3217b3056
upender-01 <bhukya.upendar.ece23@itbhu.ac.in>
2026-04-21 23:09:52 +0530

    Update App.jsx

--- frontend/myreact-app/src/App.jsx
+++ frontend/myreact-app/src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
         <Route path="/" element={<Login/>}/>
         <Route path="/dashboard" element={<Dashboard/>} />
         <Route path="/register" element={<Register />} />
+        <Route path="*" element={<Login />} />
       </Routes>
     </Router>
     </>
